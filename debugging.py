177367bb983befb707956d808c1df91ae16ef94d
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-12-19 15:30:21 +0530

    finalize Python learning module

---
+++ debugging.py
@@ -0,0 +1,23 @@
+# 1. syntax errors
+
+# 2. runtime errors
+# num = 10/0
+# print(num)
+
+# 3. logical errors
+
+# 4. Name errors
+# print(my_variable) # most recent call last
+
+# import pdb
+print("start")
+
+x = 5
+# pdb.set_trace()
+breakpoint()
+y = 0
+
+z = x/y
+
+print("end")
+
